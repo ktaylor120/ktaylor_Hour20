9eb7a77faada44be8f82341a0b51aeb465b45099
ktaylor120 <124301504+ktaylor120@users.noreply.github.com>
2023-05-10 17:39:24 -0700

    update

---
+++ Assets/Scripts/TextureScroller.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureScroller : MonoBehaviour
+{
+    public float speed = .5f;
+
+    Renderer renderer;
+    float offset;
+
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+
+    private void FixedUpdate()
+    {
+        //Increase offset based on time
+        offset += Time.deltaTime * speed;
+        //Keep offset betweeen 0 and 1
+        if (offset > 1)
+            offset -= 1;
+        //Apply the offset to the material 
+        renderer.material.mainTextureOffset = new Vector2(0, offset);
+    }
+}
